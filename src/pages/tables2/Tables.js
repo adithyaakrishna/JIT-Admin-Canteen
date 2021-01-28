ca59d6dac088621ff9187c52f5414bcf265ca23d
thedevilx <aadithya794@gmail.com>
2021-01-28 07:21:13 +0530

    Updated Tables

--- src/pages/tables2/Tables.js
+++ src/pages/tables2/Tables.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import { Grid } from "@material-ui/core";
 // import { makeStyles } from "@material-ui/styles";
 import MUIDataTable from "mui-datatables";
+import {firestore} from '../../services/firebase'
 
 // components
 import PageTitle from "../../components/PageTitle";
@@ -9,14 +10,37 @@ import PageTitle from "../../components/PageTitle";
 // import Table from "../dashboard/components/Table/Table";
 
 
-const datatableData = [
-  ["Idly", "South-Indian", "15 Plates", "₹30"],
-  ["Coffee", "Beverage", "20", "₹10"],
-  ["Tea", "Beverage", "18", "₹10"],
-  ["Lays", "Snack", "Tampa", "₹10"],
-];
+
 
 export default function Tables() {
+
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async() => {
+      setTableData([])
+      firestore.collection("foodItems").onSnapshot((snapshot) => {
+        if(!snapshot.empty){
+          snapshot.forEach((snap) => {
+            if(isMounted){
+              setTableData(prevState => [...prevState, [snap.data().foodItem, snap.data().foodCat, snap.data().price]])
+            }
+          })
+        }
+      })
+    }
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    }
+
+  },[])
+
+
+
   return (
     <>
       <PageTitle title="Current Items" />
@@ -24,8 +48,8 @@ export default function Tables() {
         <Grid item xs={12}>
           <MUIDataTable
             title="All Orders"
-            data={datatableData}
-            columns={["Food", "Type", "Quantity", "Price"]}
+            data={tableData}
+            columns={["Food", "Type", "Price"]}
             options={{
               filterType: "checkbox",
             }}
